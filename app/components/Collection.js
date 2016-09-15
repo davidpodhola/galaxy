fab6521904a49d1b57a9fda76debf6515cd67207
fbielejec <fbielejec@gmail.com>
2016-09-15 21:26:53 +0200

    paper

--- app/components/Collection.js
+++ app/components/Collection.js
@@ -1,4 +1,13 @@
 import React, {PropTypes, Component} from 'react'
+import Paper from 'material-ui/Paper';
+
+const style = {
+  height: 120,
+  width: 200,
+  margin: 20,
+  textAlign: 'center',
+  display: 'inline-block',
+};
 
 class Collection extends Component {
 
@@ -9,10 +18,10 @@ class Collection extends Component {
 
     render() {
         return (
-            <div>
+             <Paper style={style} zDepth={2}>
                 <h1> {this.props.name} </h1>
                 <h2> {this.props.info} </h2>
-            </div>
+            </Paper>
         )
     }
 
